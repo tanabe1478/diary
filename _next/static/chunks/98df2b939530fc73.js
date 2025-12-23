af02c2f62dea4ed0ac304c5ad569af77ca3ee241
tanabe1478 <tanabe1478@users.noreply.github.com>
2025-12-23 14:02:10 +0000

    deploy: 5e80a11e5f91dbaa8aa133d36d6bf01c71551dcb

--- _next/static/chunks/98df2b939530fc73.js
+++ _next/static/chunks/98df2b939530fc73.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/d10068060fb946db.js",
   "static/chunks/6bcbb7a4046ae171.js",
   "static/chunks/22efb6a829124533.js",
-  "static/chunks/8f7a7cce6c006104.css",
-  "static/chunks/turbopack-02b4e470a3c8460e.js"
+  "static/chunks/25eb38f2f154789d.css",
+  "static/chunks/turbopack-d15d09ed3aad988e.js"
 ])
